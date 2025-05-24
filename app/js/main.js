bc6c4aca39aab637b2877978b8bb974c56d6d5b1
stercorexze <stercorexze@gmail.com>
2025-05-24 14:11:52 +0200

    finished footer, fix sime troubles with accessability, started movile-nav

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,3 @@
-// import toggleSubList from "./components/toggleSubList.js";
-
-// toggleSubList();
 
 import { initModalFormElements } from "./components/modalElements.js";
 import { initModalFormHandler } from "./components/modalFormHandler.js";
